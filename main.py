22b84305c7625fdab5c98c4479ad89a793770906
Ramitha18 <92406337+Ramitha18@users.noreply.github.com>
2021-10-13 04:58:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import telebot
+
+bot=telebot.TeleBot('2096027278:AAHWHQDxmv4G1NF8K56sn3zn0kPdDijnbCo')
+@bot.message_handler(command=["start"])
+def send_welcome(message):
+  bot.reply_to(message, "Hello! I'm Ramitha's bot. How can I help you?")
+
+@bot.message_handler(command=["Hi"])
+def send_message(message):
+  bot.reply_to(message, "Hey! How you doing?")
+
+bot.polling()
