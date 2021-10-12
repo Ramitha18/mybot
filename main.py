95d1691e3a6e65fdaecb085a3be8780228723d1b
Ramitha18 <92406337+Ramitha18@users.noreply.github.com>
2021-10-13 05:15:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 import telebot
 
-bot=telebot.TeleBot('2096027278:AAHWHQDxmv4G1NF8K56sn3zn0kPdDijnbCo')
-@bot.message_handler(command=["start"])
+bot = telebot.TeleBot('2096027278:AAHWHQDxmv4G1NF8K56sn3zn0kPdDijnbCo')
+@bot.message_handler(commands=["start"])
 def send_welcome(message):
   bot.reply_to(message, "Hello! I'm Ramitha's bot. How can I help you?")
 
-@bot.message_handler(command=["Hi"])
+@bot.message_handler(commands=["Hi"])
 def send_message(message):
   bot.reply_to(message, "Hey! How you doing?")
 
