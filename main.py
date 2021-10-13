1823cb796093d713c037e2beb4177094bf2a1ce7
Ramitha18 <92406337+Ramitha18@users.noreply.github.com>
2021-10-13 05:31:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import os
 import telebot
 
-bot = telebot.TeleBot('2096027278:AAHWHQDxmv4G1NF8K56sn3zn0kPdDijnbCo')
+
+bot = telebot.TeleBot("2096027278:AAHWHQDxmv4G1NF8K56sn3zn0kPdDijnbCo")
+
+
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-  bot.reply_to(message, "Hello! I'm Ramitha's bot. How can I help you?")
+  bot.reply_to(message, "Hey! I'm Ramitha's bot. How can I help you?")
 
-@bot.message_handler(commands=["Hi"])
+
+@bot.message_handler(commands=["how"])
 def send_message(message):
-  bot.reply_to(message, "Hey! How you doing?")
+  bot.send_message(message.chat.id, "Hey! How you doing?")
+
+
 
 bot.polling()
